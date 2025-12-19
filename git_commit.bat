a5bf38e3a8eb8425a9852abb6afc0d0150f1071a
sanjanashivshankar07 <sanjanashivshankar07@gmail.com>
2025-12-19 19:03:03 +0530

    Enhanced interface with detailed analysis descriptions and file specifications

---
+++ git_commit.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Git\bin\git.exe" add -A
+"C:\Program Files\Git\bin\git.exe" commit -m "Enhanced interface with detailed analysis descriptions and file specifications"
+"C:\Program Files\Git\bin\git.exe" push origin main
--- git_push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "Removed all emojis and implemented dark/light mode toggle"
-"C:\Program Files\Git\bin\git.exe" push origin main
